0a9f4113732aac2878f364f75893fef2a065a84c
Scotty Wyatt <scotty.wyatt@worldacceptance.com>
2021-10-18 14:12:08 -0400

    added logic for search bar

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -1,44 +1,45 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from "react";
 // API
-import API from '../API';
+import API from "../API";
 
 const initialState = {
-    page: 0,
-    results: [],
-    total_page: 0,
-    total_results: 0,
-}
+  page: 0,
+  results: [],
+  total_page: 0,
+  total_results: 0,
+};
 
 export const useHomeFetch = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [state, setState] = useState(initialState);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [state, setState] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-    console.log(searchTerm)
+  console.log(searchTerm);
 
-    const fetchMovies = async (page, searchTerm = "") => {
-        try {
-            setError(false);
-            setLoading(true);
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
 
-            const movies = await API.fetchMovies(searchTerm, page);
+      const movies = await API.fetchMovies(searchTerm, page);
 
-            setState(prev => ({
-                ...movies,
-                results:
-                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-            }))
-        } catch (error) {
-            setError(true);
-        }
-        setLoading(false);
-    };
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
 
-    // Initial render
-    useEffect(() => {
-        fetchMovies(1)    
-    }, []);
+  // Initial render and search
+  useEffect(() => {
+      setState(initialState);
+    fetchMovies(1, searchTerm);
+  }, [searchTerm]);
 
-    return { state, loading, error, setSearchTerm };
-}
+  return { state, loading, error, searchTerm, setSearchTerm };
+};

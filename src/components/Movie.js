e7b49be87bb02ba063b239641e0bd9f3c8e7ef18
Scotty Wyatt <scotty.wyatt@worldacceptance.com>
2021-10-18 16:11:09 -0400

    added content to the movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,6 +8,7 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 import BreadCrumb from "./BreadCrumb";
+import MovieInfo from "./MovieInfo";
 
 // Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
@@ -20,12 +21,13 @@ const Movie = () => {
 
   const { state: movie, loading, error } = useMovieFetch(movieId);
 
-  if (loading) return <Spinner />
-  if (error) return <div>Something went wrong...</div>
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong...</div>;
 
-  return ( 
+  return (
     <>
       <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
     </>
   );
 };
